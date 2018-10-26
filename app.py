ff59f7ef77f0c08468c7ea18f4e50bb5a834d14c
panikdkernel <panikdkernel@gmail.com>
2018-10-26 17:26:49 +0530

    Add database

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\sqlite\\todo.db'
+
+db = SQLAlchemy(app)
+
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     text = db.Column(db.String(200))
+#     complete = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    return '{}'.format(request.form['todoitem'])   
 
 if __name__ == '__main__':
     app.run(debug=True)
