d3e7d552d8ece8f5ac49ee6a0f2e1b9c84a6ab03
panikdkernel <panikdkernel@gmail.com>
2018-10-27 15:12:37 +0530

    Update: now saving todos in db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,18 +7,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\sqlite\\todo.db'
 
 db = SQLAlchemy(app)
 
-# class Todo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     text = db.Column(db.String(200))
-#     complete = db.Column(db.Boolean)
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todos = Todo.query.all()
+    return render_template('index.html',todos = todos)
 
 @app.route('/add', methods=['POST'])
 def add():
-    return '{}'.format(request.form['todoitem'])   
+    todo = Todo(text = request.form['todoitem'], complete = False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
